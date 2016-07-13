22133e50058a8a2945160e38c4826476ffbaa241
dowwie <dkcdkg@gmail.com>
2016-07-13 08:59:50 -0400

    added license, updated forms

--- pyramid_yosai/forms.py
+++ pyramid_yosai/forms.py
@@ -1,4 +1,5 @@
 from yosai.core import get_current_subject
+import wtforms
 from wtforms.ext.csrf.form import SecureForm
 from pyramid.exceptions import BadCSRFToken
 
@@ -8,7 +9,7 @@ class YosaiForm(SecureForm):
     def __init__(self, formdata=None, obj=None, prefix='', context=None,
                  **kwargs):
         super().__init__(formdata, obj, prefix, **kwargs)
-        self.context = context
+        self.context = context  # a dict containing a request object
         self.csrf_token.current_token = self.generate_csrf_token(
             self.context.get('request'))
         self.obj = obj
@@ -22,3 +23,26 @@ class YosaiForm(SecureForm):
     def validate_csrf_token(self, field):
         if field.data != field.current_token:
             raise BadCSRFToken()
+
+
+def strip_filter(value):
+    return value.strip() if value else None
+
+
+class LoginForm(YosaiForm):
+    username = wtforms.StringField(
+        "Username",
+        filters=[strip_filter],
+        validators=[wtforms.validators.InputRequired(),
+                    wtforms.validators.Length(min=3)])
+
+    password = wtforms.PasswordField(
+        "Password",
+        filters=[strip_filter],
+        validators=[
+            wtforms.validators.InputRequired(),
+            wtforms.validators.Length(min=3),
+            wtforms.validators.EqualTo('confirm', message='Passwords must match')
+        ])
+
+    confirm = wtforms.PasswordField('Repeat password')
