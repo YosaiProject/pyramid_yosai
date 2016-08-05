203101c9edf81d34df716cd0e815dc796e9acfbd
dowwie <dkcdkg@gmail.com>
2016-08-05 19:19:41 -0400

    added exception for yosai init

--- pyramid_yosai/__init__.py
+++ pyramid_yosai/__init__.py
@@ -1,4 +1,5 @@
 from pyramid.tweens import EXCVIEW
+from pyramid.exceptions import ConfigurationError
 from pyramid.path import DottedNameResolver
 
 from yosai.core import AccountStoreRealm
@@ -25,31 +26,6 @@ def set_yosai(config, yosai):
     config.action(discriminator, callable=callback)
 
 
-def _parse_security_manager_settings(settings):
-    """
-    Currently, Yosai can be configured from settings specified within pyramid
-    settings INI file in conjunction with settings files specific to Yosai,
-    namely:
-        - YOSAI_CORE_SETTINGS
-        - YOSAI_CACHE_SETTINGS
-        - YOSAI_ALCHEMYSTORE_SETTINGS
-
-    yosai.accountstore
-    yosai.realms = {class: store}
-    """
-
-    # this is a temporary, hard-coded solution until INI parsing is finished
-
-    class AttributesSchema(Schema):
-        test = fields.String()
-
-    realm = AccountStoreRealm(account_store=AlchemyAccountStore())
-
-    return WebSecurityManager(realms=(realm,),
-                              cache_handler=DPCacheHandler(),
-                              session_attributes_schema=AttributesSchema)
-
-
 def yosai_from_settings(settings):
     """
     Convenience method to construct a ``Yosai`` instance, referencing paste-deploy
@@ -73,12 +49,14 @@ def includeme(config):  # pragma: no cover
     """
     :type config: :class:`pyramid.config.Configurator`
     """
+    yosai = yosai_from_settings(config.registry.settings)
+    if yosai:
+        config.registry['yosai'] = yosai
+    else:
+        msg = "pyramid_yosai could not instantiate a Yosai instance"
+        raise ConfigurationError(msg)
 
     config.add_directive('set_yosai', set_yosai)
 
     config.add_tween('pyramid_yosai.tweens.pyramid_yosai_tween_factory',
                      over=EXCVIEW)
-
-    yosai = yosai_from_settings(config.registry.settings)
-    if yosai:
-        config.registry['yosai'] = yosai
